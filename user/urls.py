517ff1e0b7efbbd80b4804542fe5dbf0e77cc171
Seyed Ali <alialhosseini.2025@gmail.com>
2026-06-30 12:05:49 +0330

    feat: added Register and Login logic

--- user/urls.py
+++ user/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from user.views import UserViewSet, AddressViewSet, AvatarViewSet
+from user.views import UserViewSet, RegisterViewSet, AddressViewSet, AvatarViewSet
 
 router = DefaultRouter()
-router.register('', UserViewSet)
-router.register('address', AddressViewSet)
-router.register('avatar', AvatarViewSet)
+router.register('', UserViewSet, basename='user')
+router.register('auth', RegisterViewSet, basename='auth')
+router.register('address', AddressViewSet, basename='address')
+router.register('avatar', AvatarViewSet, basename='avatar')
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/login', TokenObtainPairView.as_view(), name="login"),
+    path('auth/refresh/', TokenRefreshView.as_view(), name="refresh"),
 ]
